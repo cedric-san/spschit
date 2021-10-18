969a32205e3d5390a513b73dea8489c9f618ddb1
cedric-san <motovfxsan@gmail.com>
2021-10-18 23:17:54 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
 import NavBar from './Component/NavBar/NavBar';
-import Member from './Component/Members/Member';
+import Login from './Component/Login/Login';
 
 export default function App() {
   return (
     <div className="app">
       <NavBar />
+      <Login />
     </div>
   );
 }
--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './NavBar.css';
-import Login from '../Login/Login';
+import Button from '../Button/Button';
 
 const NavBar = () => {
   return (
     <div className="navBar">
       <h1 className="textStyle">SPS Chit Funds</h1>
-      <Login />
+      <Button>Sign Up</Button>
     </div>
   );
 };
